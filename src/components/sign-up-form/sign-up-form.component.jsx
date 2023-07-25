18d40e0c3bd6c87b0eb5d2770f47545178e8178e
Pavan <pansgami@gmail.com>
2023-07-25 08:29:58 +0530

    Add Sign out

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -35,7 +35,6 @@ const SignUpForm = () => {
 
     try {
       const { user } = await createAuthUserWithEmailAndPassword(email, password)
-
       await createUserDocumentFromAuth(user, { displayName })
       resetFormFields()
     } catch (error) {
